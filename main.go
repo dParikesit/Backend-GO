38be133f146dee968ec9fc4f8a6d4d09403920d4
dParikesit <dsparikesit@gmail.com>
2022-07-19 15:00:59 +0700

    init database

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/dParikesit/bnmo-backend/utils"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	var err error
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalln("Env load failed")
+	}
+
+	err = utils.InitDB()
+	if err != nil {
+		log.Fatalln("DB Connection error")
+	}
+
+	fmt.Println("Db Connected")
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
